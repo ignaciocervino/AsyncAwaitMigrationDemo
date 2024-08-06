bfa4d8fa0900f081c80690408d5547d45e5eca37
Ignacio Cervino <ignaciocervino@gmail.com>
2024-08-06 18:34:50 -0300

    Initial Commit

---
+++ AsyncAwait/AsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitApp.swift
+//  AsyncAwait
+//
+//  Created by Ignacio Cervino on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
