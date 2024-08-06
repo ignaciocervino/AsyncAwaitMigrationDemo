df67e53f8384d42b60547a8a9abc4b03bc70427b
Ignacio Cervino <ignaciocervino@gmail.com>
2024-08-06 19:04:25 -0300

    Create UI and call api function to fetchDogData

--- AsyncAwait/ContentView.swift
+++ AsyncAwait/ContentView.swift
@@ -7,13 +7,63 @@
 
 import SwiftUI
 
+class DogViewModel: ObservableObject {
+    enum DogImageError: Error {
+        case invalidImageData
+        case noImageData
+    }
+
+    func fetchDogData(completion: @escaping (Result<DogImage, Error>) -> Void) {
+        let url = URL(string: "https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg")!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(DogImageError.noImageData))
+                return
+            }
+
+            guard let dogImage =  DogImage(data: data) else {
+                completion(.failure(DogImageError.invalidImageData))
+                return
+            }
+            completion(.success(dogImage))
+        }.resume()
+    }
+}
+
 struct ContentView: View {
+    @StateObject private var dogViewModel = DogViewModel()
+    @State private var dogImage: UIImage?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let dogImage {
+                Image(uiImage: dogImage)
+                    .resizable()
+                    .scaledToFit()
+            }
+
+            Button {
+                dogViewModel.fetchDogData { result in
+                    DispatchQueue.main.async {
+                        switch result {
+                        case .success(let image):
+                            dogImage = image
+                        case .failure(let failure):
+                            break
+                        }
+                    }
+                }
+            } label: {
+                Text("Fetch Dog Image")
+                    .padding()
+                    .cornerRadius(8)
+            }
+
+
         }
         .padding()
     }
